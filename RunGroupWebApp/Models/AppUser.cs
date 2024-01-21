10e9cd468457b777851706727887f4635feac4f0
andreevskaivana <ivana.andreevska25@gmail.com>
2024-01-21 01:42:17 +0100

    Data connection and working with ssms

--- RunGroupWebApp/Models/AppUser.cs
+++ RunGroupWebApp/Models/AppUser.cs
@@ -1,7 +1,12 @@
-﻿namespace RunGroupWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RunGroupWebApp.Models
 {
     public class AppUser 
     {
+        [Key]
+        public string Id { get; set; }
+
         public int? Pace { get; set; }
         public int? Milage { get; set; }
         public Address? Address { get; set; }    
